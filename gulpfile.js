b9bc467ab5aee37ef28a6946e19d1c1f7da70bbe
zhujl <zhujialu@outlook.com>
2014-07-16 16:42:37 +0800

    build by gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('concat', function () {
+    gulp.src('./src/*.js')
+        .pipe(uglify())
+        .pipe(concat('wat.min.js'))
+        .pipe(gulp.dest('./output'));
+});
+
+gulp.task('default', ['concat']);
